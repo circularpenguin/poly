557b44e9c6c2af45d00009a39026b83130361d78
circularpenguin <vickydo_@hotmail.com>
2018-01-31 16:57:58 +0000

    Add poly_eval function

--- poly.py
+++ poly.py
@@ -4,3 +4,15 @@ def poly_diff(p):
         return [0]
     else:
         return [k*p[k] for k in range(1,deg_p+1)]
+    
+def poly_eval(p,a):
+    """Given a list representing the coefficients of a polynomial in increasing order and a, return p(a)"""
+    
+    cumsum = 0
+    
+    for n,t in zip(p, range(len(p))):
+        
+        r = n * a ** t
+        cumsum = r + cumsum
+        
+    return cumsum
